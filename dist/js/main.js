2f34e900fc2b9cdd88f838f2f3cb7a1a7e97865c
lira-power <lira13280@gmail.com>
2018-02-07 14:56:51 +1000

    8 blocks added / brends,  subscribe,  info,  ideas-for-traveling,  popular-directions,  popular-flights,  payment,  footer. Changes in vars, mixins, base, buttons, main.js, header.

---
+++ dist/js/main.js
@@ -0,0 +1,42 @@
+
+document.querySelector("body").onscroll = function () {
+    var nav, container, scrolled, help, language;
+    nav = document.querySelector(".header__nav");
+    help = document.querySelector(".header__text-help")
+    language = document.querySelector(".header__text-lang")
+    container = document.querySelector(".header__container");
+    scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrolled < 50) {
+        help.innerText = "Помощь";
+        language.innerText = "Русский (UAH - грн)";
+        if (nav.classList.contains("header__nav--visible")) {
+            nav.classList.remove("header__nav--visible");
+            nav.classList.add("header__nav--hidden");
+        }
+        if (container.classList.contains("header__container--grid-scroll") !== -1) container.classList.remove("header__container--grid-scroll");
+        return;
+    }
+    if (!nav.classList.contains("header__nav--visible")) {
+        nav.classList.remove("header__nav--hidden");
+        nav.classList.add("header__nav--visible");
+    }
+    if (!container.classList.contains("header__container--grid-scroll")) {
+        container.classList.add("header__container--grid-scroll");
+        help.innerText = '';
+        language.innerText = "Рус (грн)";
+    }
+}
+
+var links = document.querySelectorAll(".header__link")
+var linksClick = function (e) {
+    var active = document.querySelectorAll(".header__text--active");
+    for (var i = 0; i < active.length; i++) {
+        active[i].classList.remove("header__text--active");
+    }
+    e.target.querySelector('.header__text').classList.add('header__text--active');
+}
+
+for (var i = 0; i < links.length; i++) {
+    links[i].onclick = linksClick
+}
